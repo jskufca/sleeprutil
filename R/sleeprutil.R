4a9ed17b1c01e80467cb27c1edbe076b50ae6cc3
Joseph <jskufca@clarkson.edu>
2021-05-03 03:54:22 -0400

    Import magrittr piping operator.

---
+++ R/sleeprutil.R
@@ -0,0 +1,13 @@
+#' sleeprutil: Utilities for NIH (SLEEPR) grant
+#'
+#' The sleeprutil package provides data support functionality for the SLEEPR grant
+#'
+#' @section sleeprutil functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name sleeprutil
+#' @importFrom magrittr %>%
+NULL
+#> NULL
+
