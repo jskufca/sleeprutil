69c3a43f269c0b8582a17490950b607d42cba062
Joseph <jskufca@clarkson.edu>
2021-05-24 10:38:46 -0400

    Incorporated new redcap template of 5/20/2021.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,13 +2,13 @@
 
 
 # Load template for redcap database -----------------------------------
-templatefile=("D:/Dropbox/Research/Fulk_stroke/FulkStrokeRecovery/SLEEPRDataCollection_ImportTemplate_2021-04-21.csv")
-df_temp=readr::read_csv(templatefile,n_max = 0) %>%
+templatefile=("E:/Dropbox/Research/Fulk_stroke/FulkStrokeRecovery/SLEEPRDataCollection_ImportTemplate_2021-05-20.csv")
+redcap_template=readr::read_csv(templatefile,n_max = 0) %>%
   dplyr::select(1:519)
 
 
 
-usethis::use_data(df_temp, overwrite = TRUE)
+usethis::use_data(redcap_template, overwrite = TRUE,internal=TRUE)
 
 
 
