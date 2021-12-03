e2a4cba84e8bdb530d6ff47dafc5754fa9ca7bac
Joseph <jskufca@clarkson.edu>
2021-12-03 13:14:34 -0500

    Update for 2021-12-03 new template

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,12 +1,14 @@
 ## code to prepare `DATASET` dataset goes here
 
+library(dplyr,include.only = "%>%")
 
 # Load template for redcap database -----------------------------------
 #templatefile=("E:/Dropbox/Research/Fulk_stroke/FulkStrokeRecovery/SLEEPRDataCollection_ImportTemplate_2021-05-20.csv")
-templatefile=("E:/Dropbox/Research/Fulk_stroke/FulkStrokeRecovery/SLEEPRDataCollection_ImportTemplate_2021-08-30.csv")
+#templatefile=("E:/Dropbox/Research/Fulk_stroke/FulkStrokeRecovery/SLEEPRDataCollection_ImportTemplate_2021-08-30.csv")
+templatefile=here::here("data-raw","SLEEPRDataCollection_ImportTemplate_2021-12-03.csv")
 
-redcap_template=readr::read_csv(templatefile,n_max = 0) %>%
-  dplyr::select(1:549)
+redcap_template=readr::read_csv(templatefile,n_max = 0)
+
 
 
 
